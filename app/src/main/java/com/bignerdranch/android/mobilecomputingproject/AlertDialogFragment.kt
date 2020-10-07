3e3dd1a89db99694bd020287494e31b8b0734d41
hellofellowkids <adikumar02@yahoo.com>
2020-10-07 09:39:36 -0400

    STUPID OBSERVER CODE WORKS REEEEEEEEEEEEEEEEE

---
+++ app/src/main/java/com/bignerdranch/android/mobilecomputingproject/AlertDialogFragment.kt
@@ -0,0 +1,49 @@
+package com.bignerdranch.android.mobilecomputingproject
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+
+/*
+
+class AlertDialogFragment : DialogFragment() {
+
+    interface Callbacks {
+        fun onDateSelected(time: Date, protocol : Int)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Extract info from bundle
+
+        // setup callback
+
+        val ad = AlertDialog.Builder(requireContext())
+        ad.setTitle("Take a photo")
+        ad.setMessage("Would you like to take a photo?")
+
+        val positiveListener = DialogInterface.OnClickListener() {
+            fun onClick(dialog : Dialog, which : Int) {
+                // continue with option
+            }
+        }
+
+        ad.setPositiveButton(android.R.string.yes, positiveListener)
+
+
+        return ad;
+    }
+
+    companion object {
+        fun newInstance(): AlertDialogFragment {
+
+            return AlertDialogFragment()
+        }
+    }
+}
+
+ */
