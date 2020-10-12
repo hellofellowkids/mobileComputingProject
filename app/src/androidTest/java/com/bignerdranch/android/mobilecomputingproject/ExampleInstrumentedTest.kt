e6eb25374b94898e3aa6c96e7332cd04f66abdab
hellofellowkids <adikumar02@yahoo.com>
2020-10-11 21:18:33 -0400

    Working?

--- app/src/androidTest/java/com/bignerdranch/android/mobilecomputingproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/mobilecomputingproject/ExampleInstrumentedTest.kt
@@ -1,24 +1,47 @@
 package com.bignerdranch.android.mobilecomputingproject
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class MyBenchmark {
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.bignerdranch.android.mobilecomputingproject", appContext.packageName)
+    fun benchmarkSomeWork() = benchmarkRule.measureRepeated {
+        doSomeWork()
+    }
+
+    fun doSomeWork() {
+
+    }
+
+    @Test
+    fun simpleViewInflate() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val inflater = LayoutInflater.from(context)
+        val root = FrameLayout(context)
+        inflater.inflate(R.layout.fragment_task_list, root, false)
+
+        benchmarkRule.measureRepeated {
+            
+        }
     }
 }
